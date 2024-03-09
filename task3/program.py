f0013dbf3e7198f8b37326463dc78592efddf751
phanuwish chamnivigaiwech <pcha3724@uni.sydney.edu.au>
2024-03-09 19:28:06 +1100

    task3 done

--- task3/program.py
+++ task3/program.py
@@ -1,6 +1,35 @@
 def task3(message_filename, dictionary_filename, threshold):
-    #TODO
-    return ''
+    
+    with open(message_filename, 'r') as f:
+        message = f.read()
+        message_list = message.split()
+        
+    with open(dictionary_filename, 'r') as f:
+        dict = f.read()
+        dict_list = dict.split()
+        dict_list = [x.lower() for x in dict_list] # nake sure that all the words in dict is in lowercase
+    
+    # counting every matched words from message
+    count = 0
+    for word in message_list:
+        
+        # clean the word in message
+        word = word.lower()
+        ignore_char = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for char in ignore_char:
+            word = word.replace(char, "")
+        
+        if word in dict_list:
+            count += 1
+    percentage = count/len(message_list) * 100
+    
+    return_message = ""
+    if percentage >= threshold:
+        return_message = "True\n{:.2f}".format(percentage)
+    else:
+        return_message = "False\n{:.2f}".format(percentage)
+    
+    return return_message
 
 if __name__ == '__main__':
     # Example function calls below, you can add your own to test the task3 function
