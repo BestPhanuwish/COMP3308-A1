81eddc6897036a3b68bbfaf56cb36cc8a97738d0
Isaac Lau <hlau4236@uni.sydney.edu.au>
2024-03-20 12:23:39 +1100

    try again

--- task4/program.py
+++ task4/program.py
@@ -23,7 +23,7 @@ def task4(algorithm, message_filename, dictionary_filename, threshold, letters, 
     return ''
 
 def dfs():
-    
+
     return None
 
 def bfs():
@@ -34,6 +34,61 @@ def ids():
 
 def ucs():
     return None
+
+def count_words(message_list, dict_list):
+    count = 0
+    for word in message_list:
+        word = word.lower()
+        ignore_char = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for char in ignore_char:
+            word = word.replace(char, "")
+        if word in dict_list:
+            count += 1
+    percentage = count/len(message_list) * 100
+    return percentage
+
+def nodelist(filename, letters):
+    char_list = list(letters)
+    pairs = swap(char_list)
+
+    with open(filename, "r") as f:
+        text = f.read()
+
+    outputtxt = []
+    messages = 0
+    for pair in pairs:
+        newtext = ""
+        n = 0
+        for char in text:
+            if char.lower() == pair[0].lower():
+                if char.isupper():
+                    newtext += pair[1]
+                else:
+                    newtext += pair[1].lower()
+            elif char.lower() == pair[1].lower():
+                if char.isupper():
+                    newtext += pair[0]
+                else:
+                    newtext += pair[0].lower()
+            else:
+                newtext += char
+                n += 1
+        if n != len(text):
+            outputtxt.append(newtext)
+            messages += 1
+    return messages, outputtxt
+
+def swap(char_list):
+    pairs = []
+    char_list.sort()
+    for i in range(len(char_list)):
+        for j in range(i + 1, len(char_list)):
+            if char_list[i] == char_list[j]:
+                continue
+            pair = [char_list[i], char_list[j]]
+            pairs.append(pair)
+    return pairs
+
 
 if __name__ == '__main__':
     # Example function calls below, you can add your own to test the task4 function
